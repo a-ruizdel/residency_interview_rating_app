b6db8fee8522b25e2447663b567b57c5bb484a8d
a-ruizdel <a-rdl@Arlenes-MacBook-Air.local>
2015-08-25 01:30:26 -0500

    added validations

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,7 +1,11 @@
 class Candidate < ActiveRecord::Base
+  validates :candidate_lastname,:candidate_firstname, :presence => true
+  validates :candidate_lastname, :uniqueness => { :scope => :candidate_firstname }
+  validates :block,:interview_on,:usmle_1, :presence => true
+
 
   mount_uploader :photo, PhotoUploader
 
-  has_many :ratings, :class_name => "Rating", :foreign_key => "candidate_id"
+  has_many :ratings
 
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,5 @@
 class Rating < ActiveRecord::Base
+
+  validates :candidate_id,:q1,:q2,:q3,:q4, :user_id, :presence => true
   belongs_to :candidate, :class_name => "candidate", :foreign_key => "candidate_id"
 end
