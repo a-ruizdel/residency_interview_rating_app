f972c304f2ef0826909a520e018ac29801e32aeb
a-ruizdel <a-rdl@Arlenes-MacBook-Air.local>
2015-08-26 00:24:58 -0500

    adds numerically validation to ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,7 @@
 class Rating < ActiveRecord::Base
 
   validates :candidate_id,:q1,:q2,:q3,:q4, :user_id, :presence => true
+  validates :q1,:q2,:q3,:q4, numericality: { :less_than_or_equal_to => 5, :greater_than_or_equal_to => 1}
   belongs_to :candidate
   belongs_to :user
 
