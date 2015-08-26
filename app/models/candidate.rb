a5df85077e81ff08da964d1ecc2c1fea3ba4c249
a-ruizdel <a-rdl@Arlenes-MacBook-Air.local>
2015-08-25 21:26:45 -0500

    adds dropdown and changes last name to full name for candidate and add full name for user

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,10 +1,10 @@
 class Candidate < ActiveRecord::Base
-  validates :candidate_lastname,:candidate_firstname, :presence => true
-  validates :candidate_lastname, :uniqueness => { :scope => :candidate_firstname }
+  validates :candidate_lastname, :presence => true
+  validates :candidate_lastname, :uniqueness => true
   validates :block,:interview_on,:usmle_1, :presence => { :message => "howdy" }
 
   mount_uploader :photo, PhotoUploader
 
   has_many :ratings
-
+  #has_many :users, through
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,7 @@
 class Rating < ActiveRecord::Base
 
   validates :candidate_id,:q1,:q2,:q3,:q4, :user_id, :presence => true
-  belongs_to :candidate, :class_name => "candidate", :foreign_key => "candidate_id"
+  belongs_to :candidate
+  belongs_to :user
+
 end
